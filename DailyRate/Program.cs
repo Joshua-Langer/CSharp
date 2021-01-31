f0db51880c32861c03122fbc85b23a8fb3614bfd
Joshua Langer <11294501+Joshua-Langer@users.noreply.github.com>
2021-01-31 08:45:42 -0600

    Additional Projects from CSharp

---
+++ DailyRate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            Console.WriteLine("Joshua Langer's Copy");
+            double dailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter the number of days: ");
+            writeFee(calculateFee(dailyRate, noOfDays));
+        }
+
+        private void writeFee(double p) //Removed Semicolon line ender. Also removed int n from the overload to allow usage in void run()
+        {
+            Console.WriteLine("The consultant's fee is: {0}", p * 1.1);
+        }
+
+        private double calculateFee(double dailyRate, int noOfDays)
+        {
+            return dailyRate * noOfDays;
+        }
+
+        private int readInt(string p) //changed from Double to Int
+        {
+            Console.Write(p);
+            string line = Console.ReadLine();
+            return int.Parse(line);
+        }
+
+        private double readDouble(string p) //changed overload to string versus int
+        {
+            Console.Write(p);
+            string line = Console.ReadLine();
+            return double.Parse(line);
+        }
+    }
+}
---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            (new Program()).Run();
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Joshua Langer's Copy");
+            int selectionNumber = readInt("Enter a number: ");
+
+            string course = "";
+
+            switch (selectionNumber)
+            {
+                case 1:
+                    course = "IT 145";
+                    break;
+                case 2:
+                    course = "IT 200";
+                    break;
+                case 3:
+                    course = "IT 201";
+                    break;
+                case 4:
+                    course = "IT 270";
+                    break;
+                case 5:
+                    course = "IT 315";
+                    break;
+                case 6:
+                    course = "IT 328";
+                    break;
+                case 7:
+                    course = "IT 330";
+                    break;
+                default:
+                    course = "ERROR";
+                    break;
+            }
+
+            Console.WriteLine("Your Choice of " + selectionNumber + " is this course: " + course);
+        }
+
+        private int readInt(string text)
+        {
+            Console.Write(text);
+            string line = Console.ReadLine();
+            return int.Parse(line);
+        }
+    }
+}
